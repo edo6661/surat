f00050763a0bdb5f6d2806dfd0070eeda2a4012b
Edo <1122140027@global.ac.id>
2024-03-29 12:16:11 +0700

    add chromium

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,7 +2,9 @@ import puppeteer from "puppeteer";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest, res: NextResponse) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    executablePath: "C:\\Users\\muham\\AppData\\Local\\chrome-win\\chrome.exe",
+  });
   const page = await browser.newPage();
 
   await page.goto("http://localhost:3000", { waitUntil: "networkidle0" });

6b0d6e7e31041bfe7d1065387585bd4cf177a42e
Edo <1122140027@global.ac.id>
2024-03-29 14:19:01 +0700

    delete playwright

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,21 +1,22 @@
 // import puppeteer from "puppeteer";
 // import { chromium } from "playwright";
-// import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-// export const GET = async (req: NextRequest, res: NextResponse) => {
-//   const browser = await chromium.launch();
-//   const page = await browser.newPage();
+export const GET = async (req: NextRequest, res: NextResponse) => {
+  // const browser = await chromium.launch();
+  // const page = await browser.newPage();
 
-//   await page.goto("http://localhost:3000", { waitUntil: "networkidle" });
-//   const pdf = await page.pdf({ format: "A4", printBackground: true });
+  // await page.goto("http://localhost:3000", { waitUntil: "networkidle" });
+  // const pdf = await page.pdf({ format: "A4", printBackground: true });
 
-//   await browser.close();
+  // await browser.close();
 
-//   return new NextResponse(pdf, {
-//     status: 200,
-//     headers: {
-//       "Content-Type": "application/pdf",
-//       "Content-Disposition": 'attachment; filename="mypdf.pdf"',
-//     },
-//   });
-// };
+  // return new NextResponse(pdf, {
+  //   status: 200,
+  //   headers: {
+  //     "Content-Type": "application/pdf",
+  //     "Content-Disposition": 'attachment; filename="mypdf.pdf"',
+  //   },
+  // });
+  return NextResponse.json({ message: "Hello World" });
+};
